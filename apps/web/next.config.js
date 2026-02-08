9e1dd9dbb773b37adb54ba12953e5fdbc368556b
Masterluke77 <markus@mueller-bs.com>
2026-02-08 13:23:54 +0100

    neu

--- apps/web/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    transpilePackages: ["@helpa/db", "@helpa/shared"],
-    images: {
-        domains: ["localhost", "lh3.googleusercontent.com"],
-    },
-};
-
-module.exports = nextConfig;
